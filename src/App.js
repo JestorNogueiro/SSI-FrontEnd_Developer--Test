c8a7b9e46132a1437b02e6878d98e59a03a2ede8
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 01:03:32 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar/Sidebar";
+import User from "./components/user";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="flex h-full w-full">
         <Sidebar />
-        <Dashboard />
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="user" element={<User />} />
+        </Routes>
       </div>
     </div>
   );
