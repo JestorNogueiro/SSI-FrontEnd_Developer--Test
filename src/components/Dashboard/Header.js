3829e70713753300962f3198c1dc8ae293a11ab2
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 01:06:26 +0530

    added css for beautification

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -7,19 +7,26 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
-    <div className="flex fixed top-0 justify-between items-center">
-      <p>Dashboard</p>
+    <div className=" w-screen flex mx-2 my-2 justify-between text-gray-400 ">
+      <p className="text-xl font-semibold ">Dashboard</p>
 
-      <div className="right-0 mx-auto">
+      <div className="flex items-center justify-items-end space-x-3 text-sm ">
         <input
           type="text"
           placeholder="Search"
-          className="bg-transparent outline-none focus:border-none border-b-2"
+          className="bg-transparent outline-none border-b-2"
         />
+        <p className="h-[40px] w-[40px] bg-white p-1.5 rounded-full flex items-center justify-center cursor-pointer   ">
+          <SearchIcon />
+        </p>
 
-        <SearchIcon />
         <DashboardIcon />
-        <NotificationIcon />
+        <p className="flex relative items-center justify-center">
+          <NotificationIcon />
+          <p className="absolute bg-red-500 h-[20px] w-[20px]  px-1.5 rounded-full flex items-center justify-center cursor-pointer mt-[-10px] top-0 right-0 ">
+            5
+          </p>
+        </p>
         <PersonIcon />
       </div>
     </div>
