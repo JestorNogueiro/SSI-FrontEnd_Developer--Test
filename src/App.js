bbc38ec3b956f08b4d022c07caf9246bd3d8564e
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 19:28:38 +0530

    route exact

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
         <Sidebar />
 
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="user" element={<User />} />
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="user" exact element={<User />} />
         </Routes>
       </div>
     </div>
