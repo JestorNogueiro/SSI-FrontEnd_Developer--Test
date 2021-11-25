0583a4e2c898b99e0c5bb3ab9756c26cb7be7ae7
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 19:32:34 +0530

    added transition

--- src/components/user/index.js
+++ src/components/user/index.js
@@ -4,14 +4,14 @@ import Header from "../Dashboard/Header";
 const User = () => {
   return (
     <>
-      <div className=" flex flex-col h-full w-full ">
+      <div className=" flex flex-col h-full w-full p-3  ">
         <Header />
         <div
           style={{ maxWidth: "640px", marginTop: "30px" }}
-          className=" bg-white p-3 m-3 rounded-lg shadow-lg space-x-3 space-y-3"
+          className=" bg-white p-3 m-3 rounded-lg shadow-lg space-x-3 space-y-4"
         >
           <div
-            className=" p-3 bg-blue-500 text-white h-52 mb-3 rounded-md shadow-md"
+            className=" p-3 bg-blue-500 text-white h-auto mb-3 rounded-md shadow-md"
             style={{ marginTop: "-30px" }}
           >
             <h3 className="font-bold">Edit Profile</h3>
@@ -22,7 +22,7 @@ const User = () => {
               <input
                 type="text"
                 placeholder="company(disabled)"
-                className="bg-transparent outline-none border-b-2 w-48 "
+                className="bg-transparent outline-none border-b-2 w-48  "
               />
               <input
                 type="text"
@@ -40,12 +40,12 @@ const User = () => {
               <input
                 type="text"
                 placeholder="First Name"
-                className="bg-transparent outline-none border-b-2 w-72"
+                className="bg-transparent outline-none border-b-2 w-64"
               />
               <input
                 type="text"
                 placeholder="Last Name"
-                className="bg-transparent outline-none border-b-2 w-72"
+                className="bg-transparent outline-none border-b-2 w-64"
               />
             </p>
             <p className="space-x-3 my-3 text-sm">
@@ -70,7 +70,7 @@ const User = () => {
               <input
                 type="text"
                 placeholder="Lamborghini Mercy, your chick she so thirsty, I'm in the two seat lambo"
-                className="bg-transparent outline-none border-b-2 h-48 md:h-24 w-full text-sm"
+                className="bg-transparent outline-none border-b-2 h-24 sm:h-48 w-full text-sm"
               />
             </p>
             <button className=" p-2 my-3 rounded-md shadow-md transform hover:scale-110 duration-500 font-semibold bg-blue-500 text-white text ">
