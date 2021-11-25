935a9b2e13417e22f00c520b93004fc14c915582
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 17:43:49 +0530

    responsive header

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -7,7 +7,7 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
-    <div className=" w-screen flex mx-2 my-2 justify-between text-gray-400 ">
+    <div className=" w-full flex px-3 my-3 justify-between text-gray-500 ">
       <p className="text-xl font-semibold ">Dashboard</p>
 
       <div className="flex items-center justify-items-end space-x-3 text-sm ">
@@ -16,14 +16,14 @@ const Header = () => {
           placeholder="Search"
           className="bg-transparent outline-none border-b-2"
         />
-        <p className="h-[40px] w-[40px] bg-white p-1.5 rounded-full flex items-center justify-center cursor-pointer   ">
+        <p className="h-[40px] w-[40px] bg-white p-1.5 rounded-full hidden  md:flex items-center justify-center cursor-pointer   ">
           <SearchIcon />
         </p>
 
         <DashboardIcon />
         <p className="flex relative items-center justify-center">
           <NotificationIcon />
-          <p className="absolute bg-red-500 h-[20px] w-[20px]  px-1.5 rounded-full flex items-center justify-center cursor-pointer mt-[-10px] top-0 right-0 ">
+          <p className="absolute bg-red-500 h-[20px] w-[20px]  px-1.5 rounded-full flex items-center justify-center cursor-pointer mt-[-10px] top-0 right-0 text-white">
             5
           </p>
         </p>
