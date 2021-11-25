17fe02c752e4afb56c5000172beffba864b8ba84
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-25 19:39:28 +0530

    sidebar items render component

--- src/components/Sidebar/SidebarRow.js
+++ src/components/Sidebar/SidebarRow.js
@@ -1,8 +1,8 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 const SidebarRow = ({ title, Icon }) => {
   return (
-    <div className="flex p-3 cursor-pointer bg-transparent hover:bg-blue-500 rounded-md transform scale-95 text-white ">
+    <div className="flex p-3 cursor-pointer bg-transparent hover:bg-blue-500 rounded-md transform scale-95 text-white focus:bg-blue-400  ">
       {Icon && <Icon />}
       <p className="ml-3 font-bold ">{title}</p>
     </div>
