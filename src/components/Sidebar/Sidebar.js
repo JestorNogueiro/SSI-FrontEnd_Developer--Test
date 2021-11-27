1e21684041ce8870137f030c48295156f8d46a07
JestorNogueiro <jestornogueiro789@gmail.com>
2021-11-27 17:38:28 +0530

    added useState to toggle the sidebar for mobile view

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,7 +9,7 @@ import BubbleChartIcon from "@material-ui/icons/BubbleChart";
 import RoomIcon from "@material-ui/icons/Room";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import LanguageIcon from "@material-ui/icons/Language";
-// import ContentPasteIcon from "@material-ui/icons/contentP"
+import CloseIcon from "@material-ui/icons/Close";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
@@ -17,7 +17,8 @@ import { Link } from "react-router-dom";
 import { Menu } from "@material-ui/icons";
 
 const Sidebar = () => {
-  // const [open, SetOpen] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
+  // const [open, setOpen] = useState(false);
   // const location = useLocation();
   // const [bgNav, setBgNav] = useState("blue");
 
@@ -26,17 +27,31 @@ const Sidebar = () => {
   //     setBgNav("blue");
   //   }
   // }, [location]);
-
+  const handleClick = () => setOpenSidebar(true);
+  const closeSidebar = () => setOpenSidebar(false);
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex flex-col ">
       <div
-        className=" flex flex-col justify-between p-3 h-screen w-[252px] rounded-sm float-left SidebarBackground inset-y-0 left-0 transform transition duration-500 ease-in-out md:relative absolute -translate-x-full  md:translate-x-0 z-50 "
-        // style={{ }}
+        className="flex absolute md:hidden cursor-pointer hover:text-gray-500 top-3 "
+        onClick={handleClick}
       >
+        <Menu />
+      </div>
+      <div
+        className={` flex flex-col justify-between p-3 h-screen w-[252px] rounded-sm float-left SidebarBackground inset-y-0 left-0 transform transition duration-500 ease-in-out md:relative absolute -translate-x-full  md:translate-x-0 z-50  ${
+          openSidebar ? "translate-x-0" : "-translate-x-full"
+        } `}
+      >
+        <div
+          className="flex items-center justify-center md:hidden cursor-pointer hover:text-gray-500 right-0 m-2 top-1 p-2 rounded-full border border-white "
+          onClick={closeSidebar}
+        >
+          <CloseIcon />
+        </div>
         {/* <div className="border-2 border-red-600" /> */}
         <hr className="mt-5" />
         <div className=" ">
-          <Link to="/" className="active:bg-blue-300">
+          <Link to="/">
             <SidebarRow title="Dashboard" Icon={DashboardIcon} />
           </Link>
           <Link to="/user">
@@ -53,7 +68,7 @@ const Sidebar = () => {
         <div>
           <p className="bg-blue-500 font-bold rounded-md mt-2 p-2 text-white text-sm transform hover:scale-105 duration-500 cursor-pointer flex-nowrap ">
             {<CloudUploadIcon />}
-            Upgrade To PRO
+            <span>Upgrade To PRO</span>
           </p>
         </div>
       </div>
